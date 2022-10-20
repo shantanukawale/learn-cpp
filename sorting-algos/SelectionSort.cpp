7005e5cccf89e6bc76ce8f8b9d01b85e155df455
LordRishabh <116245232+LordRishabh@users.noreply.github.com>
2022-10-20 10:49:24 +0530

    Create SelectionSort.cpp

---
+++ sorting-algos/SelectionSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+// Time Complexity = O(n^2)
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements : ";
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i = 0 ; i< n-1 ; i++)
+    {
+        for( int j= i + 1 ; j < n ; j++)
+        {
+            if(arr[j]< arr[i])
+            {
+                int temp = arr[i];
+                arr[i]= arr[j];
+                arr[j]= temp;
+            }
+        }
+    }
+    for(int i= 0 ; i<n ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
